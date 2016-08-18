e8e8107c1be7644982ce29dd80819005275c3a2c
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-08-17 18:26:38 -0700

    file names are GMT based, but timestamps in log files can be in whatever time zones

--- fudge.groovy
+++ fudge.groovy
@@ -4,16 +4,16 @@ import java.util.zip.*;
 // 1.2.3.4 - - [04/Jun/2016:00:27:51 +0000] "GET / HTTP/1.1" 200 360 "-" "Mozilla/5.0 (Windows NT 6.1; rv:45.0) Gecko/20100101 Firefox/45.0"
 
 
-def format = new SimpleDateFormat("dd/MMM/yyyy");
+def format = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:SS ZZZZ");
 def of = new SimpleDateFormat("yyyyMMdd");
+of.setTimeZone(TimeZone.getTimeZone("GMT"));
 
 def o = null;   // output stream to write data to
 def prev = "";
 
 System.in.eachLine { l ->
     try {
-        def dt = l.split("[\\[\\]]")[1] // extract [04/Jun/2016:00:27:51 +0000] part
-            dt = dt.split(":")[0]
+        def dt = l.split("[\\[\\]]")[1] // extract "04/Jun/2016:00:27:51 +0000" part
             dt = format.parse(dt)
             def cur = of.format(dt) // yyyyMMdd form of the current date
             if (cur!=prev) {
